7140d2fb4ad663a0b67cf8131e68acf24a05332a
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-11-21 08:30:03 +0100

    fix(config): fixes file path being wrong

--- src/util/config.js
+++ src/util/config.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const configPath = path.join(__dirname, "../config/default.json")
+const configPath = path.join(__dirname, "../../config/default.json")
 
 class Config {
   constructor() {
