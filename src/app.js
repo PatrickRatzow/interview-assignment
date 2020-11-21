294f9186e7f45cd5e1a691f1cb1a8f7007c7a942
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-11-21 15:37:31 +0100

    improv: made query in  app.js more clear

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import QueryExport from "./services/query-export";
 
 (async () => {
-  const queryExport = new QueryExport("users", "SELECT id u, firstName AS n2, lastName, email FROM users u");
+  const queryExport = new QueryExport("users", "SELECT id, firstName, lastName, email FROM users u");
   const success = await queryExport.exportToFile();
 
   console.log(success ? "It worked!" : "It wasn't able to export this query :\\");
