6d726f40b2fbf04e06a1837a8eda959eeb185f07
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-11-21 13:28:28 +0100

    feat(query-export): added data integrity check

--- src/services/query-export.js
+++ src/services/query-export.js
@@ -1,6 +1,8 @@
 import pool from "../core/database-pool";
 import fs from "fs";
 import path from "path";
+import bufferReplace from "buffer-replace";
+import { verifyChecksum } from "./checksum";
 
 const outputPath = path.join(__dirname, "../../output")
 
@@ -26,32 +28,77 @@ export default class QueryExport {
     // TODO: This could be done smarter with a regex. Doesn't support nested queries
     const split = match.split(",")
     for (const field of split) {
-      const str = field.trim()
-      let name;
+      let name = field.trim()
+      let alias;
 
       // Deal with AS alias
-      const asAliasMatch = /AS ([\w]*)/ig.exec(str);
+      const asAliasMatch = /AS ([\w]*)/ig.exec(name);
       if (asAliasMatch != null) {
-        name = asAliasMatch[1];
+        alias = asAliasMatch[1];
       }
       // Deal with space alias
-      const spaceAliasSplit = str.split(" ")
-      if (name === undefined && spaceAliasSplit.length > 1) {
-        name = spaceAliasSplit[1];
+      const spaceAliasSplit = name.split(" ")
+      if (alias === undefined && spaceAliasSplit.length > 1) {
+        alias = spaceAliasSplit[1];
       }
 
-      if (name === undefined) {
-        name = str;
+      if (alias === undefined) {
+        alias = name;
       }
 
-      fields.push(name);
+      // Remove everything we don't want
+      name = name.split(" ")[0];
+
+      fields.push({
+        name,
+        alias
+      })
     }
 
     return fields
   }
 
-  getCountQuery() {
-    return `SELECT COUNT(*) AS rowsCount FROM ${this.getTarget()}`;
+  async getRowsCount(conn) {
+    const [rows] = await conn.execute(`SELECT COUNT(*) AS rowsCount FROM ${this.getTarget()}`);
+
+    return rows[0].rowsCount;
+  }
+
+  async getChecksum(conn) {
+    const fields = this.getFields();
+    await conn.execute("SET SESSION group_concat_max_len = 10000000");
+    const query = `SELECT MD5(GROUP_CONCAT(CONCAT_WS(',', ${fields.map(field => field.name)}))) AS checksum FROM ${this.getTarget()}`;
+    const [rows] = await conn.execute(query);
+
+    return rows[0].checksum;
+  }
+
+  async isDataValid(checksum) {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(`${outputPath}/export.csv`);
+      let chunkPos = 0;
+      const chunks = [];
+      stream.on("data", (chunk) => {
+        if (chunkPos == 0) {
+          const firstLine = chunk.indexOf("\n")
+          // Remove the header when we read this
+          chunk = chunk.slice(firstLine + 1);
+        }
+        // Remove all newlines to match MySQL's MD5 output
+        chunk = bufferReplace(chunk, "\n", ",");
+
+        chunks.push(chunk);
+
+        chunkPos++
+      })
+      stream.on("end", () => {
+        let data = Buffer.concat(chunks);
+        data = data.slice(0, data.length - 1);
+        const isIntegral = verifyChecksum(checksum, data);
+
+        resolve(isIntegral);
+      })
+    })
   }
 
   async exportToFile(chunkSize = 5) {
@@ -61,9 +108,7 @@ export default class QueryExport {
     // Start transaction
     await conn.beginTransaction();
 
-    const countQuery = this.getCountQuery(this.query);
-    const [countRows] = await conn.execute(countQuery);
-    const rowsCount= countRows[0]?.rowsCount;
+    const rowsCount = await this.getRowsCount(conn);
     if (rowsCount > 0) {
       // Find out how many chunks we need
       const chunks = Math.ceil(rowsCount / chunkSize);
@@ -72,8 +117,8 @@ export default class QueryExport {
       // Create stream to avoid having to use JSON, heavily reducing memory usage
       const stream = fs.createWriteStream(`${outputPath}/export.csv`)
       // Write a header with all the field names
-      stream.write(`${this.getFields().join(",")}\n`)
-      
+      stream.write(`${this.getFields().map(field => field.alias).join(",")}\n`)
+
       for (let i = 0; i < chunks; i++) {
         const offset = i * chunkSize;
         const [rows] = await conn.execute(chunkQuery, [chunkSize, offset]);
@@ -89,10 +134,21 @@ export default class QueryExport {
       // Finish file
       stream.end();
     }
+    
+    const checksum = await this.getChecksum(conn);
+    const isDataValid = await this.isDataValid(checksum);
 
-    await conn.commit();
+    if (isDataValid) {
+      // If valid we'll delete + commit the transction
+      await conn.execute(`DELETE FROM ${this.getTarget()}`);
+      
+      await conn.commit();
+    } else {
+      // Technically no reason to rollback, but might be handy in the future if something changes.
+      await conn.rollback();
+    }
 
-    // We're finished, release the connection back to the pool
+    // Release the connection back to the pool
     await conn.release();
   }
 }
